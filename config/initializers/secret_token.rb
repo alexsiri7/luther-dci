467c653b3b6c2ea43466d6c14ebd94197642f646
Alex <alexsiri7@gmail.com>
2013-11-03 12:12:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Auctions::Application.config.secret_key_base = '46104f65a1b5ec1ec2a0080cda8081758d1c735018d509a03a17753bcbe09e41824542f51b36ed3457bcf708de80e1548aaba3402a0a9676171e6242aaaaf5aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Auctions::Application.config.session_store :cookie_store, key: '_auctions_session'
